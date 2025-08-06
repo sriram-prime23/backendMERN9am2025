65a45653f9820fe3e8f72d3c0b3e1195602bbd7b
arunsla <arunraj.softlogic@gmail.com>
2025-08-06 09:58:11 +0530

    day 3

---
+++ index.js
@@ -0,0 +1,48 @@
+const arr = [1, 2, 3, 4, 5];
+
+// console.log(arr.join(''));
+// console.log("code ran successfully");
+
+// function array (){
+//     const array = [1, 2, 3, 4, 5];
+
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(array[i]);
+//     }   
+//     if (true){
+//         console.log("code ran successfully");
+//     }
+// }
+
+// array();
+// console.log("code ran successfully");
+
+
+
+
+// arr.forEach( (value)=>{
+//     console.log(value);
+// })
+
+// console.log("code ran successfully");
+
+// let body = document.getElementById('aa');
+
+// let h1 = document.createElement('h1');
+
+// body.appendChild(h1);
+// h1.innerHTML = 'Welcome to my MERN stack class!';
+
+
+
+// body.appendChild(document.createElement('h1'));
+// body.innerHTML = 'Welcome to my MERN stack class!';
+
+
+
+
+
+
+
+
+
