0e083417b3b36fa2fa8e13f8082c0d01928ee5cb
arunsla <arunraj.softlogic@gmail.com>
2025-08-07 10:53:50 +0530

    update

---
+++ refersher/api.js
@@ -0,0 +1,72 @@
+
+
+
+// const ar = {
+//     name: "santhosh",
+//     age: 25,
+//     city: "Bangalore"
+// }
+
+
+
+// const keys = Object.keys(ar);
+// const secondKey = keys[1];
+// const secondValue = ar[secondKey];
+
+// console.log(secondKey + " : " + secondValue);
+
+// console.log(keys);
+
+
+
+// fetch('https://api.github.com/users/arun-cloud-dev')
+
+// .then(response  =>  response.json())
+// .then ( data => {
+//     // console.log(data);
+//     console.log(data.login);
+// })
+
+
+
+
+// const ar = {
+//     name: "santhosh",
+//     age: 25,
+//     city: "Bangalore"
+// }
+
+
+
+
+// const ar = {
+//     name: "santhosh",
+//     age: 25,
+//     city: "Bangalore"
+// }
+// console.log(ar.name);
+// //object destructuring
+
+// const { name, age, city } = ar;
+// console.log(name);
+
+fetch('https://api.github.com/users/arun-cloud-dev')
+
+
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+})
+.then (data => {
+    console.log("usrname :" + data.login);
+
+    console.log("Public Repos: ",data.public_repos)
+})
+
+.catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+});
+
+
