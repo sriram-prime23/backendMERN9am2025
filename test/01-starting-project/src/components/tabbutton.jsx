2b45cbd04094292f7a772526f32c54410d0accbd
arunsla <arunraj.softlogic@gmail.com>
2025-08-18 10:15:12 +0530

    update

--- test/01-starting-project/src/components/tabbutton.jsx
+++ test/01-starting-project/src/components/tabbutton.jsx
@@ -1,6 +1,17 @@
 export default function TabButton ({children}){
+    
+
+// document.querySelector('button').addEventListener('click', () => {
+//         console.log('Button clicked!');
+//     });
+
+function handleClick(){
+    console.log("hello SLA people");
+    
+}
+
     return (
         <li>
-            <button>{children}</button></li>
+            <button onClick={handleClick}>{children}</button></li>
     )
 }
