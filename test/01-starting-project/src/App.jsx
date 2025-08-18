8331f1a07303756fb3f2ee8e103df68643105101
arunsla <arunraj.softlogic@gmail.com>
2025-08-18 09:48:48 +0530

    update

--- test/01-starting-project/src/App.jsx
+++ test/01-starting-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcepts.jsx';
+import CoreConcept from './components/coreconcepts/CoreConcepts.jsx';
 
 function App() {
   return (
