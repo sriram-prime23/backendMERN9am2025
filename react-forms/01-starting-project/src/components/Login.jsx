b806fd8b4b6c20c58d95e28ebbf70187c4a48842
arunsla <arunraj.softlogic@gmail.com>
2025-09-02 10:21:58 +0530

    update

--- react-forms/01-starting-project/src/components/Login.jsx
+++ react-forms/01-starting-project/src/components/Login.jsx
@@ -1,47 +1,48 @@
-import { useState } from "react";
+import { useState } from 'react';
+
+import { useRef } from 'react';
 
 export default function Login() {
 
 
-
-  const [enteredEmail, setEnteredEmail] = useState('');
- const [enteredPassword, setEnteredPassword] = useState('');
-
-
-// const [ enteredValues, setEnteredValues]= useState(
-//   {
-//   email:'',
-//   password:'',
-// })
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
 
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [enteredValues, setEnteredValues] = useState({
+  //   email: '',
+  //   password: '',
+  // });
 
+  function handleSubmit(event) {
+    event.preventDefault();
 
-  function handleSubmit(event){
-    event.preventDefault()
-    console.log({email:enteredEmail, password:enteredPassword});
-    
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+
+    console.log({
+      email: enteredEmail,
+      password: enteredPassword
+    });
+
   }
 
-
-
-  function handleEmailChange(event){
-    setEnteredEmail(event.target.value)
-  }
-
-  function handlePasswordChange(event){
-    setEnteredPassword(event.target.value)
-  }
-
-
-  // function handleChange(identifier, value){
-  //   setEnteredValues((prevValues)=> ({
-  //     ...prevValues, [identifier]:value,
-  //   })
-  // )
+  // function handleInputChange(identifier, value) {
+  //   setEnteredValues((prevValues) => ({
+  //     ...prevValues,
+  //     [identifier]: value,
+  //   }));
   // }
 
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
 
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -50,24 +51,35 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email"
-          onChange={handleEmailChange} 
-     />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            ref={emailRef}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" 
-
-          onChange={handlePasswordChange}
-          />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            ref={passwordRef} />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button" >Login</button>
+        <button className="button">Login</button>
       </p>
+
+
+      <div className='submitted-data'>
+
+        <h3>Submitted data </h3>
+        <p>Email : {submittedData.email}</p>
+      </div>
     </form>
   );
 }
