a9cfc86a6a6a1150fb7c7d04c9b93e1af656016a
arunsla <arunraj.softlogic@gmail.com>
2025-09-02 10:39:24 +0530

    update

--- react-forms/01-starting-project/src/components/Login.jsx
+++ react-forms/01-starting-project/src/components/Login.jsx
@@ -1,48 +1,29 @@
 import { useState } from 'react';
 
-import { useRef } from 'react';
-
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
 
-
-  const emailRef = useRef();
-  const passwordRef = useRef();
-
-
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // // const [enteredPassword, setEnteredPassword] = useState('');
-  // const [enteredValues, setEnteredValues] = useState({
-  //   email: '',
-  //   password: '',
-  // });
+  const [submittedData, setSubmittedData] = useState(null);
 
   function handleSubmit(event) {
     event.preventDefault();
-
-    const enteredEmail = emailRef.current.value;
-    const enteredPassword = passwordRef.current.value;
-
-    console.log({
-      email: enteredEmail,
-      password: enteredPassword
-    });
-
+    setSubmittedData(enteredValues); // store submitted values in state
   }
 
-  // function handleInputChange(identifier, value) {
-  //   setEnteredValues((prevValues) => ({
-  //     ...prevValues,
-  //     [identifier]: value,
-  //   }));
-  // }
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
 
-  // function handleEmailChange(event) {
-  //   setEnteredEmail(event.target.value);
-  // }
-
-  // function handlePasswordChange(event) {
-  //   setEnteredPassword(event.target.value);
-  // }
+  function handleReset() {
+    setEnteredValues({ email: '', password: '' });
+    setSubmittedData(null);
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -55,7 +36,10 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
-            ref={emailRef}
+            onChange={(event) =>
+              handleInputChange('email', event.target.value)
+            }
+            value={enteredValues.email}
           />
         </div>
 
@@ -65,21 +49,64 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
-            ref={passwordRef} />
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        <button
+          type="button"
+          className="button button-flat"
+          onClick={handleReset}
+        >
+          Reset
+        </button>
         <button className="button">Login</button>
       </p>
 
-
-      <div className='submitted-data'>
-
-        <h3>Submitted data </h3>
-        <p>Email : {submittedData.email}</p>
-      </div>
+      {/* Show submitted data below the form */}
+      {submittedData && (
+        <div className="submitted-data">
+          <h3>Submitted Data:</h3>
+          <p>Email: {submittedData.email}</p>
+          <p>Password: {submittedData.password}</p>
+        </div>
+      )}
     </form>
   );
 }
+
+
+// const [enteredEmail, setEnteredEmail] = useState('');
+// // const [enteredPassword, setEnteredPassword] = useState('');
+// const [enteredValues, setEnteredValues] = useState({
+//   email: '',
+//   password: '',
+// });
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+
+//   const enteredEmail = emailRef.current.value;
+//   const enteredPassword = passwordRef.current.value;
+
+//   console.log({
+//     email: enteredEmail,
+//     password: enteredPassword
+//   });
+
+// }
+
+
+
+// function handleEmailChange(event) {
+//   setEnteredEmail(event.target.value);
+// }
+
+// function handlePasswordChange(event) {
+//   setEnteredPassword(event.target.value);
+// }
